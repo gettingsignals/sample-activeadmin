6ee7f29baee4fc48d2ba0f6fda74764a971d7290
hina <gettingsignals@gmail.com>
2022-06-21 23:39:54 +0900

    ActionMailer / LetterOpenerWeb追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # letter opener web
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  ActionMailer::Base.delivery_method = :letter_opener
 end
